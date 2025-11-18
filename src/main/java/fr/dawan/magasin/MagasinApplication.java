a2e70fb51e8bd62b91e9fed25b516d208e5f1a29
Baylo <microsoftbaylo@laposte.net>
2025-11-18 11:41:22 +0100

    Feat: Add application properties for different environments

--- src/main/java/fr/dawan/magasin/MagasinApplication.java
+++ src/main/java/fr/dawan/magasin/MagasinApplication.java
@@ -1,13 +1,19 @@
 package fr.dawan.magasin;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
 public class MagasinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MagasinApplication.class, args);
-    }
+//        SpringApplication app = new SpringApplication(MagasinApplication.class);
+//        app.setBannerMode(Mode.OFF);
+//        app.run(args);
 
+
+    }
 }
