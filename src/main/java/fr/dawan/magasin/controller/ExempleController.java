28800df2e1cc1ebc552cf549b118a6d9d3757ff4
Baylo <microsoftbaylo@laposte.net>
2025-11-18 14:56:07 +0100

    Feat: Refactor HelloWorldController to ExempleController and add example HTML view

---
+++ src/main/java/fr/dawan/magasin/controller/ExempleController.java
@@ -0,0 +1,32 @@
+package fr.dawan.magasin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/exemple")
+public class ExempleController {
+
+    @RequestMapping("/")
+    public String exemple() {
+        return "exemple";
+    }
+
+    @RequestMapping(value = {"/testmodel", "/model"})
+    public String testModel(Model model) {
+        String message = "Test Model";
+        model.addAttribute("msg", message);
+        return "exemple";
+    }
+
+    @RequestMapping("/testmodelandview")
+    public ModelAndView testModelAndView() {
+        ModelAndView mdv = new ModelAndView("exemple");
+        mdv.addObject("msg", "TestModelAndView");
+        return mdv;
+    }
+
+}
