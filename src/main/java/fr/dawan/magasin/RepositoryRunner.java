d78c9a27b87d71c7462be09e9dc46c54dfe58418
Baylo <microsoftbaylo@laposte.net>
2025-11-20 11:46:57 +0100

    Feat: Add method to find formations by price in FormationRepository and update RepositoryRunner to demonstrate usage

--- src/main/java/fr/dawan/magasin/RepositoryRunner.java
+++ src/main/java/fr/dawan/magasin/RepositoryRunner.java
@@ -1,14 +1,27 @@
 package fr.dawan.magasin;
 
+import fr.dawan.magasin.entities.Formation;
+import fr.dawan.magasin.repositories.FormationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class RepositoryRunner implements CommandLineRunner {
+
+    private final FormationRepository formationRepository;
 
     @Override
     public void run(String... args) throws Exception {
-
+        List<Formation> formations = formationRepository.findAll();
+        for(Formation f : formations) {
+            System.out.println(f);
+        }
+        formations = formationRepository.findByPrix(new BigDecimal(800));
     }
 
 }
--- src/main/java/fr/dawan/magasin/repositories/FormationRepository.java
+++ src/main/java/fr/dawan/magasin/repositories/FormationRepository.java
@@ -3,8 +3,12 @@ package fr.dawan.magasin.repositories;
 import fr.dawan.magasin.entities.Formation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 
 public interface FormationRepository extends JpaRepository<Formation, Long> {
-
-
+//Sujet By, les prédicats
+    // Un prédicat commence toujours par une variable d'instance de l'entité
+    List<Formation> findByPrix(BigDecimal searchPrice);
 }
