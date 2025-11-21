127553ea710ef0ee53f2bf9353778ba7c67dc339
Baylo <microsoftbaylo@laposte.net>
2025-11-21 15:10:21 +0100

    Feat: Add custom error pages for 404 and 500 errors, and implement global exception handling

---
+++ src/main/java/fr/dawan/magasin/controller/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package fr.dawan.magasin.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLException.class)
+    public String handleSQLException(SQLException e, Model model) {
+        model.addAttribute("errorType", "Erreur SQL");
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(IOException.class)
+    public String handleIOException(IOException e, Model model) {
+        model.addAttribute("errorType", "Erreur I/O");
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGenericException(Exception e, Model model) {
+        model.addAttribute("errorType", "Erreur");
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
+}
